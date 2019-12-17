636e81aa47dc9263d07c7170a7ca1f2e1f70b5d0
elpat7 <aguilarpat7@gmail.com>
2019-12-17 15:18:22 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.Port || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("../app/public"));
+
+// const apiRoutes = require("./app/routing/apiRoutes");
+// app.use("/api", apiRoutes);
+
+// const htmlRoutes = require("./app/routing/htmlRoutes");
+// app.use("/", htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`listening on port http://localhost: ${PORT}`);
+})
+
+
+
+
