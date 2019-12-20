0253066f11959f7d1ceff3f6a82b4c1501618dce
elpat7 <aguilarpat7@gmail.com>
2019-12-19 19:45:34 -0800

    completed html files

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+// ===============================================================================
+// DATA
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'default' data
+// ===============================================================================
+
+var friendList = [
+    {
+        name: 'Patricio Aguilar',
+        profilePic: '',
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
+    }
+]
+
+module.exports = friendList;
